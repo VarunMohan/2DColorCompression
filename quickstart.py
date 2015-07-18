bce7836fca360e66148ca4105de5e1807b63a94d
Max Lam <malam@linkedin.com>
2015-07-17 23:52:15 -0700

    push

--- quickstart.py
+++ quickstart.py
@@ -11,28 +11,15 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v2', http=http)
 
+    reset_home()
+    print(cd(service, "Text Excerpts"))
+    print(cd(service, "Such Recursion"))
+
     items = ls(service)
     folders = [x for x in items if is_folder(x)]
     docs = [x for x in items if not is_folder(x)]
-    #print("Folders:")
-    for folder in folders:
-        pass
-        #print(folder['title'])
-    #print("Docs:")
-    for doc in docs:
-        print 'Title:', doc['title']
-    for doc in docs:
-        read(service, doc)
-        if doc['id'] == "12DCkdFVRNSTTeFQOgVRODq-wcqp_gqjpeQJD8hKFqo0":
-            delete(service, doc)
-        #if doc['id'] == "0Bza7xsC4hvoAc3RhcnRlcl9maWxl":
-        #    print(read(service, doc))
-        #print(doc['title'])
-        #read(service, doc)
-        #print(doc['id'])
-        #print doc['title'], doc['id']
-    for doc in docs:
-        print doc['title']
+
+    print(docs)
 
 if __name__ == '__main__':
     main()
--- utils/nav_utils.py
+++ utils/nav_utils.py
@@ -1,13 +1,57 @@
+import os
 from apiclient import errors
 from apiclient import http
 from apiclient.http import MediaFileUpload
 
-
 current_directory = "/"
+path_file = os.path.expanduser('~') + "/.current_dir"
+dir_id_file = os.path.expanduser('~') + "/.google_path_id"
+
+def get_cur_dir_id():
+    f = open(dir_id_file)
+    return f.read()
+
+def update_cur_dir_id(new_id):
+    f = open(dir_id_file, "w")
+    f.write(new_id)
+
+def pwd():
+    f = open(path_file)
+    return f.read()
+
+def update_pwd(new_dir):
+    f = open(path_file, "w")
+    f.write(new_dir)
+
+def reset_home():
+    update_pwd("")
+    update_cur_dir_id("")
 
 def ls(service, limit=100):
-    results = service.files().list(maxResults=limit).execute()
-    return results.get('items', [])
+    cur_id = get_cur_dir_id()
+    results = ""
+    if not cur_id:
+        results = service.files().list().execute()
+        return results.get('items', [])
+    else:
+        children = service.children().list(folderId=cur_id).execute()
+        results = []
+        for child in children.get('items', []):
+            results.append(service.files().get(fileId=child['id']).execute())
+        return results
+
+def cd(service, folder):
+    cur_id = get_cur_dir_id()
+    results = ls (service)
+    new_id = None
+    for file in results:
+        if file['title'] == folder:
+            new_id = file['id']
+    if not new_id:
+        return False
+    update_pwd(pwd() + "/" + folder)
+    update_cur_dir_id(new_id)
+    return True
 
 def is_folder(f):
     return "application/vnd.google-apps.folder" == f['mimeType']
